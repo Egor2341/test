efca99c813896b6206c050566bd465ac9e78f93e
Egor2341 <95289626+Egor2341@users.noreply.github.com>
2022-05-31 17:55:59 +0500

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,15 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
